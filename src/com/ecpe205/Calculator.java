9a015cb60e3b75dc0ca5cb513f43b2b510bd7fce
John Raphael <Johnrserantes424@gmail.com>
2022-05-10 22:50:52 +0800

    Serantes_J Unit Test

--- src/com/ecpe205/Calculator.java
+++ src/com/ecpe205/Calculator.java
@@ -1,26 +1,47 @@
 package com.ecpe205;
 
 public class Calculator {
-    public double sum( double a, double b ) {
+    public double sum(double a, double b) {
         return a + b;
     }
-    public boolean isEven (int value) {
+
+    public boolean isEven(int value) {
         return value % 2 == 0;
     }
-    public boolean isOdd (int value) {
+
+    public boolean isOdd(int value) {
         return value % 2 == 1;
     }
 
     //create a method that computes base x power of y
     //use parameterizedTest methodsource - 5 sets to test
+    public double ans(int x, int y) {
+        return Math.pow(x, y);
+    }
 
     //create a method that computes for the factorial of a value
     //use parameterizedTest- 5 values to test
+    public long factorial(int f) {
+        return (1 > f) ? 1 : f * factorial(f - 1);
+    }
 
     //create a method that checks if a given string is a palindrome
     //use parameterizedTest - 5 values to test
 
+
     //create a method that accepts an array of integer values, the method then sorts the value in ascending order
     //** you may choose whatever sort data structure (bubble sort, shell sort,...)
     //use parameterizedTest methodSource
+
+    void bubbleSort(int b[]) {
+        int n = b.length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (b[j] > b[j + 1]) {
+                    // swap arr[j+1] and arr[j]
+                    int temp = b[j];
+                    b[j] = b[j + 1];
+                    b[j + 1] = temp;
+                }
+    }
 }
--- src/com/ecpe205/CalculatorTest.java
+++ src/com/ecpe205/CalculatorTest.java
@@ -41,6 +41,16 @@ class CalculatorTest {
         assertEquals(5, calc.sum(3,2) );
         assertEquals(14, calc.sum(5,9) );
     }
+    //Test of number 1
+    @Test
+    @DisplayName("Ans of Base X Power Y")
+    void shouldBaseXPowerY1 (){
+
+        assertEquals(8, calc.ans(2,3) );
+        assertEquals(27, calc.ans(3,3) );
+        assertEquals(1, calc.ans(12,0) );
+        assertEquals(1, calc.ans(5,0) );
+    }
 
     @ParameterizedTest
     @ValueSource(ints = {1,2,3,4,5})
@@ -54,6 +64,32 @@ class CalculatorTest {
         assertEquals(a + b, calc.sum(a, b));
     }
 
+    //1. Test of base X power of Y
+    @ParameterizedTest
+    @MethodSource("AnsForBasePower")
+    void shouldBaseXPowerY(int x, int y) { assertEquals( Math.pow(x, y), calc.ans(x, y)); }
+
+    //2. Test of factorial
+    @ParameterizedTest
+    @ValueSource(ints =  {120, 1, 720, 40320, 6})
+    void shouldGetFact(int f) {
+        assertEquals(120, calc.factorial(5));
+        assertEquals(1, calc.factorial(1));
+        assertEquals(720, calc.factorial(6));
+        assertEquals(40320, calc.factorial(8));
+        assertEquals(6, calc.factorial(3));
+
+    }
+    //3. Test of Palindrome
+
+
+    //4. Test of Sort
+    @ParameterizedTest
+    @MethodSource("AnsForBasePower")
+    void ShouldSort(int [ ] array) { assertEquals( calc.bubbleSort(array)
+    }
+
+
     static Stream<Arguments> sumInputValues () {
         return Stream.of(
                 Arguments.of(1,2),
@@ -64,11 +100,15 @@ class CalculatorTest {
         );
     }
 
-    static Stream<Arguments> arrayOfIntegerSets () {
+    //1. Test of base X power of Y
+    static Stream<Arguments> AnsForBasePower () {
         return Stream.of(
-                Arguments.of(new int[]{1,2,3,4,1}),
-                Arguments.of(new int[]{7,1,5,4,1}),
-                Arguments.of(new int[]{7,2,8,3,4})
+                Arguments.of(2,3),
+                Arguments.of(6,4),
+                Arguments.of(12,4),
+                Arguments.of(1,7),
+                Arguments.of(1,0)
         );
     }
+
 }
